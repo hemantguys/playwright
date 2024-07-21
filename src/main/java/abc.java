e6d84654925a12271c305fb90fd91a1ec10b7960
Pankaj <hanumant@gmail.com>
2024-07-21 22:10:33 +0530

    my playwright first commit

---
+++ src/main/java/abc.java
@@ -0,0 +1,22 @@
+import com.microsoft.playwright.*;
+
+public class abc {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            // Launch a Chromium browser instance
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            // Create a new browser context
+            Browser.NewContextOptions contextOptions = new Browser.NewContextOptions();
+            BrowserContext context = browser.newContext(contextOptions);
+            // Open a new page
+            Page page = context.newPage();
+            // Navigate to a website
+            page.navigate("https://www.google.com");
+            // Take a screenshot
+            //   page.screenshot(new Page.ScreenshotOptions().setPath("example.png"));
+            Thread.sleep(3000);
+            // Close the browser
+             browser.close();
+        }
+    }
+}
